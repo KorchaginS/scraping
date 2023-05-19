1452d0fb36f5967ee46c5786fcfde8ca8bc35dc7
KorchaginS <KorchaginSV1995@yandex.ru>
2023-05-19 21:29:36 +0300

    Сайт начал выкидывать капчу. Попытка обойти через selenium

--- salomon.py
+++ salomon.py
@@ -2,14 +2,37 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import json
+import time
+from proxy import proxyCon
+from selenium import webdriver
+from selenium.webdriver.chrome.service import  Service
 
 headers = {
     'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 }
 
 def main():
 
+    s = Service(executable_path='C:\chromedriver\chromedriver')
+
+    driver = webdriver.Chrome(
+        executable_path= 'C:\chromedriver\chromedriver'
+    )
+    driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',{
+        'source': '''
+            delete cdc_adoQpoasnfa76pfcZLmcfl_Array;
+            delete cdc_adoQpoasnfa76pfcZLmcfl_Promise;
+            delete cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
+            delete cdc_adoQpoasnfa76pfcZLmcfl_Object;
+            delete cdc_adoQpoasnfa76pfcZLmcfl_Proxy;
+        '''
+    })
+    driver.maximize_window()
+
+    driver.get('https://www.salomon.com/en-us/shop/men/shoes.html')
+    time.sleep(4)
+    driver.close()
     result = []
     r = requests.get(url=f'https://www.salomon.com/en-us/shop/men/shoes.html', headers=headers)
 
@@ -21,13 +44,16 @@ def main():
         a = card.find('a')
         dict['Name'] = a.get_text()
         dict['Link'] = a.get('href')
+        dict['Price'] = getPrice(dict['Link'])
         result.append(dict)
+        time.sleep(2)
 
-
+    time.sleep(3)
     pageCount = 0
     for page in range(1000):
         r = requests.get(url=f'https://www.salomon.com/en-us/shop/men/shoes.html?p={page}', headers=headers)
         print(f'Вычисляем количество страниц. Проверено:{page} ')
+        time.sleep(3)
         if r.url != f'https://www.salomon.com/en-us/shop/men/shoes.html?p={page}':
             pageCount = page - 1
             break
@@ -86,13 +112,21 @@ def main():
             resDict = {}
             resDict['Name'] = names[position]
             resDict['Link'] = links[position]
+            resDict['Price'] = getPrice(resDict['Link'])
             result.append(resDict)
-
+        time.sleep(3)
     with open('salomon_result.json', 'w', encoding='utf-8') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
 
 
-
+def getPrice(url):
+    r = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+    try:
+        price = soup.find('div', class_='product-information_row').find('span').get_text().replace('$', '')
+    except:
+        price = soup.find('div', class_='product-sidebar-price').find('span').get_text().replace('$', '')
+    return price
 
 if __name__ == '__main__':
     main()
